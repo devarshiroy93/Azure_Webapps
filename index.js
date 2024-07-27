c8af7adcf1c0e96ab1ffe58f4c6765d59394de25
devarshiroy93 <devarshiroy93@gmail.com>
2024-07-27 11:26:17 +0530

    changes

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.get('/',(req,res)=>{
     res.send("HELLO WORLD")
 });
 
-app.get('/base', ()=>{
-    return res.send("BASE ROUTE");
-})
+
 app.listen(process.env.PORT || 3001 , ()=>{
     console.log(`Listening to server at ${process.env.PORT || 3001}`)
 });
